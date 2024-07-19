0092f2b3f5dd836998f9537ea64338bdfd8b38b8
Mark Snyder <Snyder4000@yahoo.com>
2024-07-19 09:28:46 -0400

    intial project setup

---
+++ setup/src/main/java/com/revature/Main.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        This main method is available if you want to tinker with things, but the Planetarium
+        setup content is located in the Setup class in the test directory
+     */
+    }
+}
